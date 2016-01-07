09a77cf00832d5e8e3e2b9720274eb75e06548c6
Byron Peña <byronpenna@gmail.com>
2016-01-06 21:38:12 -0600

    finalizacion

--- application/views/Noticias/agregar.php
+++ application/views/Noticias/agregar.php
@@ -15,7 +15,28 @@
 		
 	</header>
 	<div class="cuerpo row marginNull">
-		
+		<form id="frmNoticia">
+			<label>Titulo</label>	
+			<input class="form-control" name="txtTitulo" class="txtTitulo">
+			<label>Contenido</label>
+			<div class="row marginNull">
+				<div id="editor">
+					<h1>Hello world!</h1>
+					<p>I'm an instance of <a href="http://ckeditor.com">CKEditor</a>.</p>
+				</div>
+			</div>
+		</form>
 	</div>
+	<footer class='row marginNull'>
+		
+	</footer>
+	<!-- Otra carga -->
+		<?php 
+			// scripts universales
+			$this->load->view("parts/scripts.php")
+		?>
+		<!-- editor -->
+		<script type="text/javascript" src=<?php echo base_url("Content/js/Welcome/functions.js") ?> ></script>
+		<script type="text/javascript" src=<?php echo base_url("Content/js/Welcome/script.js") ?> ></script>
 </body>
 </html>
